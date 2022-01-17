3b40086040f5c2df75d6ddbf13304a232bd5d88d
ramonjd <ramonjd@gmail.com>
2022-01-17 15:05:28 +1100

    Initial commit and directory structure.

---
+++ functions.php
@@ -0,0 +1,10 @@
+
+<?php
+/**
+ * A_Sentence_Too_Far functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package A_Sentence_Too_Far
+ * @since A_Sentence_Too_Far 1.0
+ */
